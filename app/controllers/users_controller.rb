a9f277b9b2619886dd932cac0498c073743e3dab
Granit Haxhaj <hax.granit@gmail.com>
2016-03-08 10:25:54 -0500

    Make Rubocop happy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,13 +48,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :time_zone, :location)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :first_name, :last_name, :time_zone, :location)
+  end
 end
