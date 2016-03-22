6442a46778b72232baeb1c1c21b3085d0ad6d4c5
Granit Haxhaj <hax.granit@gmail.com>
2016-03-22 11:27:01 -0400

    Devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users
   root 'static#home'
-  get 'signup' => 'users#new'
-  post 'signup' => 'users#create'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
 
   namespace :api do
     namespace :v1 do
