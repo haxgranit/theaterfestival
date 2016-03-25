88102c1104d242f22b89e8f9dfb0ad22c970078a
Granit Haxhaj <hax.granit@gmail.com>
2016-03-25 10:12:37 -0400

    Add some social features

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,6 @@
 class Artist < ActiveRecord::Base
   belongs_to :user
+  acts_as_followable
+  acts_as_likeable
+  acts_as_mentionable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  acts_as_follower
+  acts_as_followable
+  acts_as_liker
+  acts_as_likeable
+  acts_as_mentionable
+  acts_as_mentioner
+
   before_save { email.downcase! }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
