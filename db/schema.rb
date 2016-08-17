f55650e39775a4f79e872542062485fbbf8a4692
Granit Haxhaj <hax.granit@gmail.com>
2016-08-16 21:46:49 -0400

    DB updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 64) do
+ActiveRecord::Schema.define(version: 65) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 64) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "type"
+    t.boolean  "original"
   end
 
   add_index "credits", ["artist_id"], name: "index_credits_on_artist_id", using: :btree
