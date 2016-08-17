ccc2634ddb12264f4ff7fdaec1ad185d7e89b76e
Granit Haxhaj <hax.granit@gmail.com>
2016-08-17 00:02:17 -0400

    Spin claims into a concern

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :permissions
   end
 
+  concern :claimable do
+    patch :claim
+  end
+
   concern :social do
     post :like
     post :follow
@@ -24,28 +28,27 @@ Rails.application.routes.draw do
   get :profile, to: 'users#show'
 
   resources :artists do
-    patch :claim
-    concerns [:permissible, :social, :search]
+    concerns [:permissible, :social, :search, :claimable]
   end
 
   resources :companies do
-    concerns [:permissible, :social, :search]
+    concerns [:permissible, :social, :search, :claimable]
   end
 
   resources :festivals do
-    concerns [:permissible, :social, :search]
+    concerns [:permissible, :social, :search, :claimable]
   end
 
   resources :productions do
-    concerns [:permissible, :social, :search]
+    concerns [:permissible, :social, :search, :claimable]
   end
 
   resources :theaters do
-    concerns [:permissible, :social, :search]
+    concerns [:permissible, :social, :search, :claimable]
   end
 
   resources :venues do
-    concerns [:permissible, :social, :search]
+    concerns [:permissible, :social, :search, :claimable]
   end
 
   resources :activities
