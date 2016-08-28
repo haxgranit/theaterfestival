c99d93dc25a9a60abfa4bf46bbc8ca68d72cd2fc
Granit Haxhaj <hax.granit@gmail.com>
2016-08-28 15:08:54 -0400

    Clean routes, get rid of LiveReload

--- config/routes.rb
+++ config/routes.rb
@@ -62,52 +62,11 @@ Rails.application.routes.draw do
   resources :festival_production_links
   resources :production_showtime_links
 
-  resources :production_credits, controller: :credits, type: 'ProductionCredit'
-  resources :writing_credits, controller: :credits, type: 'WritingCredit'
-  resources :staff_credits, controller: :credits, type: 'StaffCredit'
   root 'static#home'
-
-
-
 
   namespace :api do
     namespace :v1 do
       jsonapi_resources :users
     end
   end
-
-  namespace :admin do
-    resources :users
-    resources :artists
-    resources :artist_social_metadata
-    resources :companies
-    resources :company_festival_links
-    resources :company_metadata
-    resources :company_production_links
-    resources :company_social_metadata
-    resources :credits
-    resources :festivals
-    resources :festival_production_links
-    resources :pictures
-    resources :press_items
-    resources :productions
-    resources :production_metadata
-    resources :production_showtime_links
-    resources :reviews
-    resources :showtimes
-    resources :showtime_accessibility_metadata
-    resources :showtime_ticket_metadata
-    resources :theaters
-    resources :theater_metadata
-    resources :venues
-    resources :follows
-    resources :likes
-    resources :mentions
-    resources :production_credits
-    resources :staff_credits
-    resources :writing_credits
-
-    root to: "users#index"
-  end
-
 end
