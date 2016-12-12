9ff937f6a82aa15bd67591dc349071c32bf3396f
Granit Haxhaj <hax.granit@gmail.com>
2016-12-12 09:48:57 -0500

    Make the location search work properly

--- app/controllers/concerns/search_setup.rb
+++ app/controllers/concerns/search_setup.rb
@@ -4,12 +4,21 @@ module SearchSetup
   private
 
   def set_production_search
-    conditions = params[:conditions] || {'upcoming.someday': true}
+    conditions = {'upcoming.someday': true}
     conditions.delete_if { |_,v| v == "0" || v.blank? }
-    if session[:geo_location].present?
+    if conditions['showtimes.venue.city'].present? || conditions['showtimes.venue.state'].present? || conditions['showtimes.venue.zip'].present?
+      city, state, zip = conditions['showtimes.venue.city'], conditions['showtimes.venue.state'], conditions['showtimes.venue.zip']
+      loc = Geokit::Geocoders::MultiGeocoder.geocode("#{city}, #{state} #{zip}")
+      if loc.success
+        conditions[:location] = { near: [loc.lat, loc.lng], within: '25mi'  }
+      end
+    elsif session[:geo_location].present?
       loc = session[:geo_location]
-      conditions[:location] = { near: [loc.lat, loc.lng], within: '25mi'  }
+      if loc.present?
+        conditions[:location] = { near: [loc.lat, loc.lng], within: '2000mi'  }
+      end
     end
+
     aggs = ['closing_soon', 'upcoming.today', 'upcoming.tomorrow', 'upcoming.weekend',
             'size.broadway', 'size.offbroadway', 'size.black_box',
            'guaranteed_price']
