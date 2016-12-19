1475ceae41d6c96d80e3bf7a0e8d02079afb0e02
Granit Haxhaj <hax.granit@gmail.com>
2016-12-19 07:11:05 -0500

    Festival steps definition

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,4 +1,10 @@
 class Festival < ActiveRecord::Base
+  cattr_accessor :form_steps do
+    %w(timing festival_title company festival_data festival_shows festival_info)
+  end
+
+  attr_accessor :form_step
+
   include Permissible
   include HasAlbum
   include PublicActivity::Common
