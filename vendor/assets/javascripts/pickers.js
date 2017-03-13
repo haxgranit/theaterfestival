c6ad6f4ad52ecdd044608fb034fa690dd3adeaef
Granit Haxhaj <hax.granit@gmail.com>
2017-03-13 08:58:03 -0400

    Fix dates

--- vendor/assets/javascripts/pickers.js
+++ vendor/assets/javascripts/pickers.js
@@ -1,4 +1,4 @@
-$(document).on('ready turbolinks:load changeDate page:change cocoon:after-insert', function() {
+$(document).on('ready turbolinks:load changeDate page:change cocoon:after-insert ajax:complete', function() {
   $('.datetimepicker').datetimepicker({
     // put here your custom picker options, that should be applied for all pickers
     icons: {
